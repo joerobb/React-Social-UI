3c8a69ef094ca78be20ccb2ed70d2ae81f0df45f
robb_joe <robb_joe@hotmail.co.uk>
2020-02-04 13:34:22 +0000

    Updates

---
+++ precache-manifest.487a7cd72c8e2577499b051fedf47586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926ca08f248081517c90326eb6ed6a26",
+    "url": "/React-Social-UI/index.html"
+  },
+  {
+    "revision": "af39c6f0fe50de99b8c3",
+    "url": "/React-Social-UI/static/css/main.9156843f.chunk.css"
+  },
+  {
+    "revision": "7eb79639e371e9abeb93",
+    "url": "/React-Social-UI/static/js/2.53a5f982.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Social-UI/static/js/2.53a5f982.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af39c6f0fe50de99b8c3",
+    "url": "/React-Social-UI/static/js/main.cbfe146a.chunk.js"
+  },
+  {
+    "revision": "8260f495faaa7c594944",
+    "url": "/React-Social-UI/static/js/runtime-main.2e151db4.js"
+  },
+  {
+    "revision": "847b8d950aa35abc1a6b3c803396181d",
+    "url": "/React-Social-UI/static/media/lego.847b8d95.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/socialui/precache-manifest.b9357f822c59b6199cb3f8b57ade4543.js"
+  "/React-Social-UI/precache-manifest.487a7cd72c8e2577499b051fedf47586.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/socialui/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Social-UI/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
